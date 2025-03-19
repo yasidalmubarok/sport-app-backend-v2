1d45f67b1e48dc8148dd11e3eec69f6570445d91
unknown <yasidalmubarok@gmail.com>
2025-03-19 22:28:07 +0700

    Modified: format uuid

--- dto/userOwner.go
+++ dto/userOwner.go
@@ -1,5 +1,14 @@
 package dto
 
+import (
+	"sport-app-backend/models"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var UserOwnerRole = "owner"
+
 type RegisterUserOwnerRequest struct {
 	Name        string `json:"name" validate:"required,min=3,max=30"`
 	Username    string `json:"username" validate:"required,min=3,max=30"`
@@ -7,7 +16,12 @@ type RegisterUserOwnerRequest struct {
 	Password    string `json:"password" validate:"required,min=6,max=30"`
 }
 
-type UserOwnerResponse struct {
+type LoginUserOwnerRequest struct {
+	Username string `json:"username" validate:"required,min=3,max=30"`
+	Password string `json:"password" validate:"required,min=6,max=30"`
+}
+
+type RegisterOwnerResponse struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Username    string `json:"username"`
@@ -15,4 +29,23 @@ type UserOwnerResponse struct {
 	Role        string `json:"role"`
 	CreatedAt   string `json:"created_at"`
 	UpdatedAt   string `json:"updated_at"`
+}
+
+func (uor *RegisterUserOwnerRequest) NewUserOwner() models.UserOwner {
+	id := uuid.New().String()
+	rawCreatedAt := time.Now().Format(time.RFC3339)
+	created, _ := time.Parse(time.RFC3339, rawCreatedAt)
+	rawUpdatedAt := time.Now().Format(time.RFC3339)
+	updated, _ := time.Parse(time.RFC3339, rawUpdatedAt)
+
+	return models.UserOwner{
+		ID:          id,
+		Name:        uor.Name,
+		Username:    uor.Username,
+		PhoneNumber: uor.PhoneNumber,
+		Password:    uor.Password,
+		Role:        UserOwnerRole,
+		CreatedAt:   created,
+		UpdatedAt:   updated,
+	}
 }
