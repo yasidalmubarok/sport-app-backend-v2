8224523a1b52603455723a3593e30c1ecafc1b5f
unknown <yasidalmubarok@gmail.com>
2025-03-19 21:44:28 +0700

    Modified: setup directory

---
+++ helper/helper.go
@@ -0,0 +1,82 @@
+package helper
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type Response struct {
+	Meta Meta        `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+type Meta struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
+}
+
+func APIResponse(message string, code int, status string, data interface{}) Response {
+	meta := Meta{
+		Message: message,
+		Code:    code,
+		Status:  status,
+	}
+
+	jsonResponse := Response{
+		Meta: meta,
+		Data: data,
+	}
+
+	return jsonResponse
+}
+
+func FormatValidationError(err error) []string {
+	var errorsList []string
+
+	// Check if the error is of type ValidationErrors
+	if validationErrors, ok := err.(validator.ValidationErrors); ok {
+		for _, e := range validationErrors {
+			errorsList = append(errorsList, e.Error())
+		}
+		return errorsList
+	}
+
+	// Check if the error is a JSON UnmarshalTypeError
+	var unmarshalTypeError *json.UnmarshalTypeError
+	if errors.As(err, &unmarshalTypeError) {
+		errorsList = append(errorsList, fmt.Sprintf("Field '%s' has an invalid type", unmarshalTypeError.Field))
+		return errorsList
+	}
+
+	// Check if the error is a JSON SyntaxError
+	var syntaxError *json.SyntaxError
+	if errors.As(err, &syntaxError) {
+		errorsList = append(errorsList, "Invalid JSON syntax")
+		return errorsList
+	}
+
+	// Handle general errors
+	errorsList = append(errorsList, err.Error())
+	return errorsList
+}
+
+func ValidatePhoneNumber(phone string) error {
+	re := regexp.MustCompile(`^(08|628)[0-9]{8,11}$`)
+	if !re.MatchString(phone) {
+		return errors.New("invalid phone number")
+	}
+	return nil
+}
+
+func ValidateEmail(email string) error {
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !re.MatchString(email) {
+		return errors.New("invalid email address")
+	}
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sport-app-backend/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db := config.ConnectDB()
+	router := gin.Default()
+
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Bisa disesuaikan
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+	// Tambahkan router di sini...
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Printf("Server running on port %s", port)
+	router.Run(fmt.Sprintf(":%s", port))
+}
